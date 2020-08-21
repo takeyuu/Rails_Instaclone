92a2a25c1480d26215d1b186793773c2b0c8c005
takeoka <tokih4ru@gmail.com>
2020-08-21 14:32:03 +0900

    Pictureモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_052339) do
+ActiveRecord::Schema.define(version: 2020_08_21_053033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.text "article"
+    t.text "image"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 2020_08_21_052339) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pictures", "users"
 end
