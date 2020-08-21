fb9fb6bb10f3d71632bc250bab78d1431a3236eb
takeoka <tokih4ru@gmail.com>
2020-08-21 15:09:54 +0900

    Comentモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_060514) do
+ActiveRecord::Schema.define(version: 2020_08_21_060807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "picture_id"
+    t.text "coment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
