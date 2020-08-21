b38c6b7daf7704c8c9a5ed5670639ea253895abf
takeoka <tokih4ru@gmail.com>
2020-08-21 15:29:27 +0900

    アソシエーション定義

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
+  has_many :coments, dependent: :destroy
+  has_many :coment_users, through: :coments, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :pictures
+  has_many :likes, dependent: :destroy
+  has_many :like_pictures, through: :likes, source: :picture
+  has_many :coments, dependent: :destroy
 end
