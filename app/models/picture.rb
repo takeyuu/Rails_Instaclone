8aa6ac621d128ca9e2878386516d34101df94d0f
takeoka <tokih4ru@gmail.com>
2020-08-22 15:50:39 +0900

    画像アップロード機能追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ApplicationRecord
+  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
