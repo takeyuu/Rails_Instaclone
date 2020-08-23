6c7a2cc8f86fab2f56c0fdefb2a8b445e987e0eb
takeoka <tokih4ru@gmail.com>
2020-08-23 16:50:28 +0900

    ログインログアウト機能作成

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,16 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def authenticate_user
+    if current_user == nil
+      flash[:danger] = 'ログインが必要です'
+      redirect_to new_session_path
+    end
+  end
+end
