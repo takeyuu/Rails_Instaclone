f77b5edd2a87ee7924b3b4fe0b0be79346544099
takeoka <tokih4ru@gmail.com>
2020-09-03 18:49:01 +0900

    確認メール機能作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,6 +26,7 @@ class PicturesController < ApplicationController
     @picture = current_user.pictures.build(picture_params)
     @user = current_user
     if @picture.save
+      PostMailer.post_mail(@picture).deliver
       redirect_to user_path(current_user.id)
     else
       flash.now[:danger] = 'エラー！'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   root 'pictures#top'
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update] do
+    resources :likes, only: [:show]
+  end
   resources :pictures do
     collection do
       post :confirm
