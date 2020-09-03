303da2002178be700d95028e5123d4c8dab485ea
takeoka <tokih4ru@gmail.com>
2020-09-04 01:19:47 +0900

    修正

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,6 +1,6 @@
 class PostMailer < ApplicationMailer
   def post_mail(post)
     @post = post
-    mail to: @post.user.email, subject: "投稿の確認メール"
+    mail to: 'tokih4ru@gmail.com', subject: "投稿の確認メール"
   end
 end
