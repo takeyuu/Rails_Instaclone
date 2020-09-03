77dcc42d9d2ee113d2ed7432bfda4d6afa594341
takeoka <tokih4ru@gmail.com>
2020-09-04 01:36:01 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,15 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'intense-reef-53317.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.SendGrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,16 +102,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = { host: 'intense-reef-53317.herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    address: "smtp.SendGrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
 end
